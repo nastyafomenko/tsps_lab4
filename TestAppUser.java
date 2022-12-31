a2f8b08307daa2878083cb1240e5e8145d431686
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2022-12-31 17:23:38 +0200

    test authorization and registration

---
+++ TestAppUser.java
@@ -0,0 +1,34 @@
+package project.library;
+
+//import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import java.net.MalformedURLException;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestAppUser 
+{
+		AppUser apUs = new AppUser();
+	    @Test
+    public void checkRightAuthorization() throws MalformedURLException
+    {
+    	assertEquals("right password", apUs.authorization("anna12lr"));
+    }
+    @Test
+    public void checkWrongAuthorization() throws MalformedURLException
+    {
+    	assertEquals("wrong password", apUs.authorization("Bnna12lr"));
+    }
+    @Test
+    public void checkRegistration() throws MalformedURLException
+    {
+    	assertEquals("User registred", apUs.register("anOlks19", "Богданова Анна Олександрівна", "12.04.1999", "+380958324651"));
+    }
+}
+
+
